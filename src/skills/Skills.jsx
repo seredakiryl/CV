35dcb61638711f359f08d21737e7bc23309164d5
seredaKiryl <seredakiryl@gmail.com>
2023-03-15 22:26:27 +0300

    skills block new design

--- src/skills/Skills.jsx
+++ src/skills/Skills.jsx
@@ -3,36 +3,35 @@ import styleContainer from "../common/styles/Container.module.css";
 import Skill from "./skill/Skill";
 import Title from "../common/components/title/Title";
 import {
-	GitSkill,
-	HtmlSkill,
-	JavaScriptSkill,
-	JestSkill,
-	ReactSkill,
-	ReduxSkill,
-	SassSkill,
-	StoryBookSkill,
-	TypeScriptSkill,
-} from "./skill/StyleSkills";
+  SiReact,
+  SiRedux,
+  SiJavascript,
+  SiTypescript,
+  SiGit,
+  SiJest,
+  SiSass,
+  SiStorybook,
+  SiHtml5,
+} from "react-icons/si";
 
 function Skills() {
-	return (
-		<div className={s.skillsBlock}>
-			<div className={`${styleContainer.container} ${s.skillsContainer}`}>
-				<Title text={"Skills"} />
-				<div className={s.skills}>
-					<Skill title={"React"} style={ReactSkill} description={" is one of the most important web development languages used today."} />
-					<Skill title={"Redux"} style={ReduxSkill} description={"JavaScript is one of the most important web development languages used today."} />
-					<Skill title={"TypeScript"} style={TypeScriptSkill} description={"Css is one of the most important web development languages used today."} />
-					<Skill title={"JavaScript"} style={JavaScriptSkill} description={"Css is one of the most important web development languages used today."} />
-					<Skill title={"StoryBook"} style={StoryBookSkill} description={"Css is one of the most important web development languages used today."} />
-					<Skill title={"Jest"} style={JestSkill} description={"Css is one of the most important web development languages used today."} />
-					<Skill title={"Git"} style={GitSkill} description={"Css is one of the most important web development languages used today."} />
-					<Skill title={"Sass"} style={SassSkill} description={"Css is one of the most important web development languages used today."} />
-					<Skill title={"Html"} style={HtmlSkill} description={"Css is one of the most important web development languages used today."} />
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className={s.skillsBlock}>
+      <div className={`${styleContainer.container} ${s.skillsContainer}`}>
+        <Title text={"Skills"} />
+        <div className={s.skills}>
+          <Skill title={"React"} Icon={SiReact} />
+          <Skill title={"Redux"} Icon={SiRedux} />
+          <Skill title={"TypeScript"} Icon={SiTypescript} />
+          <Skill title={"JavaScript"} Icon={SiJavascript} />
+          <Skill title={"StoryBook"} Icon={SiStorybook} />
+          <Skill title={"Jest"} Icon={SiJest} />
+          <Skill title={"Git"} Icon={SiGit} />
+          <Skill title={"Sass"} Icon={SiSass} />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Skills;
--- src/skills/skill/Skill.jsx
+++ src/skills/skill/Skill.jsx
@@ -1,13 +1,14 @@
 import s from "./Skill.module.scss";
 
-function Skill(props) {
-	return (
-		<div className={s.skill}>
-			<div style={props.style} className={s.icon}></div>
-			<h3 className={s.title}> {props.title}</h3>
-			<span className={s.description}>{props.description}</span>
-		</div>
-	);
+function Skill({ Icon, title }) {
+  return (
+    <div className={s.skill}>
+      <div className={s.skill__content}>
+        <Icon className={s.icon} />
+        <h3 className={s.title}> {title}</h3>
+      </div>
+    </div>
+  );
 }
 
 export default Skill;
